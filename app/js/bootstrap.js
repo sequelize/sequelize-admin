1d3c88b89de3bd31e52222880383336d6726eeae
Sascha Depold <sascha@depold.com>
2012-12-28 00:19:39 +0100

    removed old assets folder

--- app/js/bootstrap.js
+++ app/js/bootstrap.js
@@ -1,43 +1,47 @@
-// Configure the AMD module loader
-requirejs.config({
-  // The path where your JavaScripts are located
-  baseUrl: './js/',
+$(function() {
+  var endpoint = $('[data-endpoint]').data('endpoint')
 
-  // Specify the paths of vendor libraries
-  paths: {
-    jquery:     'vendor/jquery-1.8.2',
-    underscore: 'vendor/underscore-1.4.2',
-    backbone:   'vendor/backbone-0.9.2',
-    handlebars: 'vendor/handlebars-1.0.rc.1',
-    text:       'vendor/require-text-2.0.3',
-    chaplin:    'vendor/chaplin-1.0.0-pre-59cac06',
-    bootstrap:  'vendor/bootstrap/main'
-  },
+  // Configure the AMD module loader
+  requirejs.config({
+    // The path where your JavaScripts are located
+    baseUrl: endpoint + '/app/js/',
 
-  // Underscore and Backbone are not AMD-capable per default,
-  // so we need to use the AMD wrapping of RequireJS
-  shim: {
-    underscore: {
-      exports: '_'
+    // Specify the paths of vendor libraries
+    paths: {
+      backbone:   'vendor/backbone-0.9.2',
+      chaplin:    'vendor/chaplin-1.0.0-pre-59cac06',
+      handlebars: 'vendor/handlebars-1.0.rc.1',
+      jquery:     'vendor/jquery-1.8.2',
+      text:       'vendor/require-text-2.0.3',
+      underscore: 'vendor/underscore-1.4.2',
+      bootstrap:  'vendor/bootstrap/main'
     },
-    backbone: {
-      deps: ['underscore', 'jquery'],
-      exports: 'Backbone'
-    },
-    handlebars: {
-      exports: 'Handlebars'
+
+    // Underscore and Backbone are not AMD-capable per default,
+    // so we need to use the AMD wrapping of RequireJS
+    shim: {
+      underscore: {
+        exports: '_'
+      },
+      backbone: {
+        deps:    ['underscore', 'jquery'],
+        exports: 'Backbone'
+      },
+      handlebars: {
+        exports: 'Handlebars'
+      }
     }
-  }
 
-  // For easier development, disable browser caching
-  // Of course, this should be removed in a production environment
-  , urlArgs: 'bust=' +  (new Date()).getTime()
-})
-
-if (document.title !== 'Buster.JS') {
-  // Bootstrap the application
-  require([ 'bootstrap', 'corporate_portal' ], function(Bootstrap, CorporatePortal) {
-    var app = new CorporatePortal()
-    app.initialize()
+    // For easier development, disable browser caching
+    // Of course, this should be removed in a production environment
+    , urlArgs: 'bust=' +  (new Date()).getTime()
   })
-}
+
+  // if (document.title !== 'Buster.JS') {
+  //   // Bootstrap the application
+  //   require([ 'bootstrap', 'corporate_portal' ], function(Bootstrap, CorporatePortal) {
+  //     var app = new CorporatePortal()
+  //     app.initialize()
+  //   })
+  // }
+})
